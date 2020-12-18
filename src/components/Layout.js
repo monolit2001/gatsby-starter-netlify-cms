6c7abdb99a5f57941664b8cac849f7fd66d58a67
monolit2001 <taomingstudio@gmail.com>
2020-12-18 21:02:15 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,8 @@ import Navbar from '../components/Navbar'
 import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
+import "../src/styles/style.css"
+import './src/styles/style.css'
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
